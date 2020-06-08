b5985e0df9a2f78e3ea67c3b23e14c6d7257203c
Paul Bradley <bradleypauld@gmail.com>
2020-06-08 16:55:46 -0500

    add some packages and questions

---
+++ util/questions.js
@@ -0,0 +1,18 @@
+function startingQuestion() {
+    return [
+        {
+            type: "list",
+            name: "action",
+            message: "What would you like to do?",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "Update an employee role"
+            ]
+        }
+    ];
+}
